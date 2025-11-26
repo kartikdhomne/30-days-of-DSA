f732eb40d32f780b4d6a60b5a7bb1d7b0727eec9
Kartik Dhomne <kartikdhomne1997@gmail.com>
2025-11-26 23:59:06 +0530

    Two Sum Sorted Input Two Pointers

--- Day-2.js
+++ Day-2.js
@@ -2,23 +2,47 @@
 
 let arr = [2, 7, 4, 9, 0, 3, 6];
 let arr2 = [2, 3, 4, 5, 6, 7];
-target = 11;
+target = 5;
 
+// Method 1( Hashmap )
 
 const TwoSum = (arr, target) => {
     if (arr.length === 0) return;
 
-    const map = new Map()
+    const map = new Map();
 
     for (let i = 0; i < arr.length; i++) {
-        let complement = target - arr[i]
+        let complement = target - arr[i];
 
         if (map.has(complement)) {
-            return [map.get(complement), i]
+            return [map.get(complement), i];
         }
-        map.set(arr[i], i)
+        map.set(arr[i], i);
     }
+};
 
-}
+console.log(TwoSum(arr, target));
+
+// Mathod 2 ( Two Pointers only if input array is sorted)
+
+const TwoSumTwoPointers = (arr, target) => {
+    if (arr.length === 0) return;
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+
+        if (sum === target) {
+            return [left, right];
+        } else if (sum < target) {
+            return left++;
+        } else {
+            return right--;
+        }
+    }
+    return null;
+};
 
 console.log(TwoSum(arr2, target));
