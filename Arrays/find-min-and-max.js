ce6ab7f8d6ca8f699e8869cdec16d04b3409ca6f
Kartik Dhomne <kartikdhomne1997@gmail.com>
2025-12-04 23:58:11 +0530

    Find Min and Max

---
+++ Arrays/find-min-and-max.js
@@ -0,0 +1,19 @@
+let arr = [1, 4, 2, 6, 8, 3, 9]
+
+
+function MaxVal(arr) {
+    let res = [...arr].sort((a, b) => b - a);
+    return res[0];
+}
+console.log(MaxVal(arr))
+
+function MinVal(arr) {
+    let res = [...arr].sort((a, b) => a - b);
+    return res[0];
+}
+console.log(MinVal(arr))
+
+const Max = arr => Math.max(...arr);
+console.log(Max(arr))
+const Min = arr => Math.min(...arr);
+console.log(Min(arr))
