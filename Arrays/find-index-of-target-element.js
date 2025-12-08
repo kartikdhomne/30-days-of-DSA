06e41cf638bb2f0c1924d5cc0e8f09d869247308
Kartik Dhomne <kartikdhomne1997@gmail.com>
2025-12-08 23:19:14 +0530

    find-index-of-target-element

---
+++ Arrays/find-index-of-target-element.js
@@ -0,0 +1,28 @@
+// Find index of target element(linear search)
+
+let arr = [3, 2, 5, 8, 4, 7]
+
+target = 3
+
+
+// Binary Search
+function searchInsert(nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+};
+
+
+console.log(searchInsert(arr, 5))
